cf012293f8612f37f48a2eec7a727404f7fbd83a
goddim <132663924+goddim@users.noreply.github.com>
2024-02-05 20:41:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ import mysql.connector
 from datetime import datetime
 
 app = Flask(__name__)
-db_host = os.environ.get('DB_HOST')
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_database = os.environ.get('DB_NAME')
+db_host=os.environ.get(DB_HOST)
+db_user=os.environ.get(DB_USER)
+db_password=os.environ.get(DB_PASSWORD)
+db_database=os.environ.get(DB_NAME)
 
 # Подключение к базе данных MySQL
 db = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_database,
-    autocommit=True
+host=db_host,
+user=db_user,
+password=db_password,
+database=db_database,
+autocommit=True )
+cursor = db.cursor()
+
+# SQL-запрос для создания таблицы в БД
+create_table_query = f"""
+CREATE TABLE IF NOT EXISTS {db_database}.requests (
+id INT AUTO_INCREMENT PRIMARY KEY,
+request_date DATETIME,
+request_ip VARCHAR(255)
 )
+"""
+cursor.execute(create_table_query)
 
 @app.route('/')
 def index():
     # Получение IP-адреса пользователя
     ip_address = request.headers.get('X-Forwarded-For')
-
-    # Создание объекта cursor
-    cursor = db.cursor()
 
     # Запись в базу данных
     now = datetime.now()
@@ -34,9 +41,6 @@ def index():
     values = (current_time, ip_address)
     cursor.execute(query, values)
     db.commit()
-
-    # Закрытие объекта cursor
-    cursor.close()
 
     return f'TIME: {current_time}, IP: {ip_address}'
 
